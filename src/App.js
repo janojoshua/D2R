6432cfe02c072aad0162fe4ce88f828d33ad7f29
joshua <joshuajano@yahoo.com>
2021-12-09 16:36:58 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Genggam tanganku sayang dekat denganku peluk diriku berdiri tegak didepan aku</code> and save to reload.
         </p>
         <a
           className="App-link"
