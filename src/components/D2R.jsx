746b0b4a77f8788af259a3c609405c0f561d46cf
joshua <joshuajano@yahoo.com>
2021-12-09 22:13:14 +0900

    [D2R] change tab name commit

--- src/components/D2R.jsx
+++ src/components/D2R.jsx
@@ -143,13 +143,25 @@ export default function D2R(){
                       {/* <h3>Rve<span class="fg-primary">Tive.</span></h3> */}
                     </div>
                     <div class="col-lg-3 py-3">
-                      
+                      {/* <h5>Contact Information</h5> */}
+                      {/* <p>301 The Greenhouse, Custard Factory, London, E2 8DY.</p> */}
+                      {/* <p>Email: example@mail.com</p> */}
+                      {/* <p>Phone: +00 112323980</p> */}
                     </div>
                     <div class="col-lg-3 py-3">
-                      
+                      {/* <h5>Company</h5> */}
+                      {/* <ul class="footer-menu"> */}
+                        {/* <li><a href="#">Career</a></li> */}
+                        {/* <li><a href="#">Resources</a></li> */}
+                        {/* <li><a href="#">News & Feed</a></li> */}
+                      {/* </ul> */}
                     </div>
                     <div class="col-lg-3 py-3">
-                      
+                      {/* <h5>Newsletter</h5>
+                      <form action="#">
+                        <input type="text" class="form-control" placeholder="Enter your email" />
+                        <button type="submit" class="btn btn-primary btn-sm mt-2">Submit</button>
+                      </form> */}
                     </div>
                   </div>
 
@@ -161,7 +173,10 @@ export default function D2R(){
                     </div>
                     <div class="col-md-6 text-right">
                       <div class="sosmed-button">
-                        
+                        {/* <a href="#"><span class="mai-logo-facebook-f"></span></a>
+                        <a href="#"><span class="mai-logo-twitter"></span></a>
+                        <a href="#"><span class="mai-logo-youtube"></span></a>
+                        <a href="#"><span class="mai-logo-linkedin"></span></a> */}
                       </div>
                     </div>
                   </div>
