ba5036a7a412991deea423f5a66b69baacf00f98
joshua <joshuajano@yahoo.com>
2021-12-09 17:18:37 +0900

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import D2R from './components/D2R';
 function App() {
   return (
-    <D2R />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>Genggam tanganku sayang dekat denganku peluk diriku berdiri tegak didepan aku</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
